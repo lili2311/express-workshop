bd7afdde7c8943818f0d725aae15757da8dcfc77
Liliana Kastilio <kastilio.liliana@gmail.com>
2016-07-02 14:58:46 +0100

    Wiriting and posting blog posts

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs= require('fs');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(4000, function () {
 	console.log('Server is listening on port 4000 and ready to accept requests');
@@ -7,14 +11,31 @@ app.listen(4000, function () {
 
 app.use('/', express.static('public'));
 
-app.get('/node', function (request, response) {
-	response.send('This is NODE endpoint!');
+app.get('/get-posts', function (request, response) {
+	response.sendFile(__dirname + '/data/posts.json');
 });
 
-app.get('/girls', function (request, response) {
-	response.send('This is GIRLS endpoint!');
+app.post('/create-post', function (request, response) {
+	console.log('/create-post endpoint ======> ', request.body);
+	//read current posts in file
+	var parsedFile;
+
+	fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+		var parsedFile = JSON.parse(file);
+    	console.log("currently in the file:", parsedFile);
+		parsedFile[Date.now()] = request.body.blogpost;
+    	fs.writeFile(__dirname + '/data/posts.json', JSON.stringify(parsedFile), (err) => {
+			if (err) throw err;
+			console.log('It\'s saved!');
+			response.redirect('/');
+
+		});
+	});
+
+
 });
 
-app.get('/create-post', function (request, response) {
-	console.log('/create-post endpoint ======> ' + request.body);
+fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+	var parsedFile = JSON.parse(file);
+    console.log(file.toString());
 });
